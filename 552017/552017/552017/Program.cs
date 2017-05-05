501c3067e2b75c70380f761ba908143755e63f49
Paul Hawkins <hawkinsanthonypaul@gmail.com>
2017-05-05 12:58:16 -0400

    Finished Project.

--- 552017/552017/552017/Program.cs
+++ 552017/552017/552017/Program.cs
@@ -10,6 +10,40 @@ namespace _552017
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("List out numbers seperated by commas. This program will see if it adds up to zero.");
+            string numbers = Console.ReadLine();
+            numbers.Replace("\n", string.Empty);
+            string[] splitNums = numbers.Split(',');
+            int length = splitNums.Length;
+            List<int> num = new List<int>();
+            int i = 0;
+            while (i != length){
+                num.Add(Int32.Parse(splitNums[i]));
+                i++;
+            }
+            
+            int b = 0;
+            int result = 0;
+            int listcount = num.Count;
+            while(b != listcount)
+            {
+                result = result + num[b];
+                b++;
+
+            }
+            if(result == 0)
+            {
+                Console.WriteLine("Yes, it equals zero.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("sike it doesn't");
+                Console.ReadLine();
+            }
+
+
+
         }
     }
 }
