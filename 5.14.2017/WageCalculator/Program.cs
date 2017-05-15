04014e63b09cd188b2c7c3018e283aa20a9bd88d
ItsPaulHawkins <hawkinsanthonypaul@gmail.com>
2017-05-14 22:27:20 -0400

    Project done.

--- 5.14.2017/WageCalculator/Program.cs
+++ 5.14.2017/WageCalculator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using System.Threading;
 
 namespace WageCalculator
 {
@@ -10,6 +12,28 @@ namespace WageCalculator
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("What is your hourly wage?");
+            double wage = double.Parse(Console.ReadLine());
+            wage = wage / 60;
+            wage = wage / 60;
+            double staticWage = wage;
+            while (true)
+            {
+                sleep();
+                Console.Clear();
+                Console.WriteLine("You have made " + wage);
+                wage = wage + staticWage;
+
+            }
+            
+
+
         }
+        public static void sleep()
+        {
+            Thread.Sleep(1000);
+        }
+
     }
 }
